9895a2a9d1e56d70a62e989017715c2ae56a3de1
Ethan <Ethan-McGregor>
2018-11-29 08:02:48 -0800

    class starter code

--- project6/project6/node.py
+++ project6/project6/node.py
@@ -1,1 +1,35 @@
+class Node:
+
+    def __init__(self, key, value=None):
+        self.__key = key
+        self.__value = value
+        self.__leftChild = None
+        self.__rightChild = None
+
+    def getKey(self):
+        return self.__key
+
+    def setKey(self, key):
+        pass
+
+    def getValue(self):
+        return self.__value
+
+    def setValue(self, value):
+        self.__value = value
+
+    def getLeftChild(self):
+        return self.__leftChild
+
+    def setLeftChild(self, childNode):
+        self.__leftChild = childNode
+
+    def getRightChild(self):
+        return self.__rightChild
+
+    def setRightChild(self, childNode):
+        self.__rightChild = childNode
+
+    def isLeaf(self):
+        return self.getLeftChild() == None and self.getRightChild() == None
 
--- project6/project6/tree.py
+++ project6/project6/tree.py
@@ -1,5 +1,64 @@
-class binaryTree:
-    pass
+class BinarySearchTree:
+    def __init__(self):
+        self.__root = None
+        self.__size = 0
+
+    def getCount(self):
+        return self.__size
+
+    def isEmpty(self):
+        return self.__size == 0
+
+    def get(self, key):
+        currentNode = self.__root
+        while currentNode != None:
+            if currentNode.getkey() == key:
+                return currentNode.getValue()
+            elif currentNode.getkey() > key:
+                currentNode = currentNode.getLeftChild()
+            else:
+                currentNode = currentNode.getRightChild()
+        return None
+
+    def __getItem__(self, key):
+        return self.get(key)
+
+    def put(self, key, value):
+        if self.isEmpty():
+            self.__root  = Node(key, value)
+            self.__size += 1
+            return
+        currentNode = self.__root
+        while currentNode != None:
+            if currentNode.getkey() == key:
+                currentNode.setValue(value)
+                return
+            elif currentNode.getkey() > key:
+                if currentNode.getLeftChild() == None:
+                    newNode = Node(key, value)
+                    currentNode.setLeftChild(newNode)
+                    break
+                else:
+                    currentNode = currentNode.getLeftChild()
+            else:
+                if currentNode.getRightChild() == None:
+                    newNode = Node(key, value)
+                    currentNode.setRightChild(newNode)
+                    break
+                else:
+                    currentNode = currentNode.getRightChild()
+        self.__size += 1
+
+    def __setItem__(self, key, data):
+        self.put(key, data)
+
+    def inOrderTraversal(self, func):
+        self.__inOrderTraversalRec(self.__root, func)
+
+    def __inOrderTraversalRec__(self, theNode, func):
+        self.__inOrderTraversalRec__(theNode.getLeftChild(), func)
+        func(theNode.getValue())
+        self.inOrderTraversal(theNode.getRightChild(), func)
 
 
 
